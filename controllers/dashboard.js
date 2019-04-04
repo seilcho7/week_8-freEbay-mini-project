da69eb02ff5f1c8f90a135e36abaae63a6f2342e
Seil Cho <seilcho7@hotmail.com>
2019-04-04 15:01:04 -0400

    login to dashboard

--- controllers/dashboard.js
+++ controllers/dashboard.js
@@ -1,12 +1,17 @@
-function dashboardPage(req, res) {
+const User = require('../models/users');
+
+async function dashboardPage(req, res) {
+    console.log(req.body.email);
+    const userData = await User.getById(req.session.user);
+    console.log(userData);
+    console.log("That was the usserrrrrrrrrrrrrrrrrr");
+
     res.render('dashboard', {
         locals: {
-            message: "DASHBOAAARD"
+            message: `Welcome ${userData.firstName} ${userData.lastName}`
         }
     });
 }
-
-
 
 module.exports = {
     dashboardPage
